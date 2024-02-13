32f3b1cde993ef728c71ce56b124779962bb3cca
thayrov <thayrovg@gmail.com>
2024-02-13 14:11:19 -0600

    💡 [ADD]: update root reducer

--- front/src/store/root-reducer.ts
+++ front/src/store/root-reducer.ts
@@ -1,11 +1,6 @@
-import { combineReducers } from '@reduxjs/toolkit';
-import { persistReducer } from 'redux-persist';
+import {combineReducers} from '@reduxjs/toolkit';
+import {persistReducer} from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
-import { sessionReducer } from './slices/session';
-import { listReducer } from './slices/list';
-import { optionsReducer } from './slices/options';
-import { trainingListReducer } from './slices/trainingList';
-import { trainingUpdateReducer } from './slices/trainingUpdate';
 
 const persistConfig = {
   key: 'root',
@@ -13,13 +8,7 @@ const persistConfig = {
   whitelist: ['session'],
 };
 
-const combinedReducers = combineReducers({
-  session: sessionReducer,
-  options: optionsReducer,
-  list: listReducer,
-  trainingList: trainingListReducer,
-  trainingUpdate: trainingUpdateReducer,
-});
+const combinedReducers = combineReducers({});
 
 const persistedReducer = persistReducer(persistConfig, combinedReducers);
 
